53980b9e54791474d95f3f6cc59062c78a7f0375
re3ae6 <re3ae6@gmail.com>
2026-08-11 10:25:16 +0330

    Delete www/src/js directory

--- www/src/js/core/simple.js
+++
@@ -1,9 +0,0 @@
-export function calculateSimpleNextStake({payout, targetProfit, streakLoss, floor, balance, stopLossBalance}){
-  const requiredReturn = streakLoss + targetProfit;
-  let stake = requiredReturn / payout;
-  if(stake < floor) stake = floor;
-  if(balance < floor) return {stake:0, reason:'insufficient-balance'};
-  const projected = balance - stake;
-  if(stake <= 0 || projected < stopLossBalance) return {stake:0, reason:'stoploss'};
-  return {stake, reason:'ok'};
-}
